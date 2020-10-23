c2900bc4ffdfdae4ccf2753312345adab7f81789
liannedls <liannedelasalle@gmail.com>
2020-10-23 16:48:03 -0500

    small change to name

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Ski Lesson Planner</Link>
+        <li className="navbar-brand">Ski Lesson Planner</li>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
