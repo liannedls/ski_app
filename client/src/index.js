7d721e173a5c2c63d0f2befa15dac45cdc08546f
liannedls <liannedelasalle@gmail.com>
2020-11-02 21:39:15 -0600

    production

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if (process.env.NODE_ENV === "production"{
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
