bd06ac273bca23b3fdc09dd7b6a90ce5cc6e9c88
liannedls <liannedelasalle@gmail.com>
2020-12-03 17:09:01 -0600

    add see all functionality

--- routes/exercises.js
+++ routes/exercises.js
@@ -6,9 +6,23 @@ router.route('/').get((req, res) => {
   num = parseInt(req.query.num)
   console.log(num);
   //Exercise.find({group : req.query.group}).limit(num)
-  Exercise.aggregate([ { $match: { group : req.query.group, age: req.query.age, skill: req.query.skill } } , { $sample: { size : num } } ])
-  .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json('Error: ' + err));
+  if(req.query.group == "All" ){
+    Exercise.aggregate([ { $match: {age: req.query.age, skill: req.query.skill } } , { $sample: { size : num } }])
+    .then(exercises => res.json(exercises))
+      .catch(err => res.status(400).json('Error: ' + err));
+  }
+  else{
+    Exercise.aggregate([ { $match: {group: req.query.group, age: req.query.age, skill: req.query.skill } } , { $sample: { size : num } }])
+    .then(exercises => res.json(exercises))
+      .catch(err => res.status(400).json('Error: ' + err));
+  
+}
+});
+
+router.route('/all').get((req, res) => {
+  Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error: ' + err));  
 });
 
 module.exports = router;
