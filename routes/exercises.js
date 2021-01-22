17f682769ea9b8ec9decc28424aad2efd15180bc
liannedls <liannedelasalle@gmail.com>
2021-01-21 22:31:01 -0600

    change how data is queried

--- routes/exercises.js
+++ routes/exercises.js
@@ -5,18 +5,10 @@ let Exercise = require('../models/exercise.model');
 router.route('/').get((req, res) => {
   num = parseInt(req.query.num)
   console.log(num);
-  //Exercise.find({group : req.query.group}).limit(num)
-  if(req.query.group == "All" ){
-    Exercise.aggregate([ { $match: {age: req.query.age, skill: req.query.skill } } , { $sample: { size : num } }])
+  console.log(req.query.age)
+  Exercise.aggregate([ { $match: {$or: [{age: req.query.age, skill: req.query.skill, group: req.query.group},{age: "Any", skill: req.query.skill, group: req.query.group}, {age: req.query.age, skill: req.query.skill, group: "All"}, {age: "Any", skill: req.query.skill, group: req.query.group}]}} , { $sample: { size : num } }])
     .then(exercises => res.json(exercises))
       .catch(err => res.status(400).json('Error: ' + err));
-  }
-  else{
-    Exercise.aggregate([ { $match: {group: req.query.group, age: req.query.age, skill: req.query.skill } } , { $sample: { size : num } }])
-    .then(exercises => res.json(exercises))
-      .catch(err => res.status(400).json('Error: ' + err));
-  
-}
 });
 
 router.route('/all').get((req, res) => {
